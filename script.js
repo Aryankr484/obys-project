6b80cc0a4c2f121c8670fefb8bc5f7ec5e38ca1a
Aryan Kumar <aryankr484@gmail.com>
2024-07-14 13:20:20 +0530

    page1 done

--- script.js
+++ script.js
@@ -1,41 +1,62 @@
-var tl=gsap.timeline()
-tl.from(".line h1",{
+function loadingAnimation() {
+  var tl = gsap.timeline();
+  tl.from(".line h1", {
+    y: 100,
+    stagger: 0.25,
+    duration: 0.6,
+    delay: 0.5,
+  });
+  tl.from("#line1-part1", {
+    opacity: 0,
+    onStart: function () {
+      var h5timer = document.querySelector("#line1-part1 h5");
+      var grow = 0;
+      setInterval(function () {
+        if (grow < 100) {
+          h5timer.innerHTML = grow++;
+        } else {
+          h5timer.innerHTML = grow;
+        }
+      }, 20);
+    },
+  });
+  tl.to(".line h2", {
+    opacity: 1,
+    animationName: "anime",
+  });
+
+  tl.to("#loader", {
+    opacity: 0,
+    duration: 0.4,
+    delay: 0,
+  });
+  tl.from("#page1", {
+    delay: 0.2,
+    y: 1600,
+    opaciy: 1,
+    duration: 0.7,
+  });
+  tl.to("#loader", {
+    display: "none",
+  });
+  tl.from("nav",{
+    opacity:0,
+    duration:0.5
+  })
+  tl.from("#hero1 h1, #hero2 h1, #hero3 h2,#hero3 h3, #hero4 h1",{
     y:100,
     stagger:0.25,
-    duration:0.6,
-    delay: 0.5
-})
-tl.from("#line1-part1",{
-    opacity:0,
-    onStart:function(){
-        var h5timer=document.querySelector("#line1-part1 h5")
-            var grow=0
-            setInterval(function(){
-                if(grow<100){
-                    h5timer.innerHTML=grow++
-                }
-                else{
-                    h5timer.innerHTML=grow
-                }
-        },20)
-    }
-})
-tl.to(".line h2",{
-    opacity:1,
-    animationName:"anime"
-})
-
-tl.to("#loader",{
-    opacity:0,
-    duration:0.4,
-    delay:3
-})
-tl.from("#page1",{
-    delay:0.2,
-    y:1600,
-    opaciy:1,
-    duration:0.7
-})
-tl.to("#loader",{
-    display:"none"
-})
+    duration:0.6
+  })
+}
+function cursorAnimation(){
+  document.addEventListener("mousemove",function(dets){
+    gsap.to("#crsr",{
+      left:dets.x,
+      top:dets.y
+    });
+  });
+  Shery.makeMagnet("#nav-part2 h4");
+}
+loadingAnimation()
+cursorAnimation()
